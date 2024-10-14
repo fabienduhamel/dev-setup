65428fd0a60dfde612f0fc53315385214727db74
fabienduhamel <fabienduhamel@users.noreply.github.com>
2024-10-14 23:36:17 +0200

    added atuin config

--- link.sh
+++ link.sh
@@ -3,21 +3,20 @@
 # replaces the target file with the file in this directory
 # and creates a symlink
 
-function link
-{
-    set +e
-    rm "$2"
-    set -e
-    ln -s "$(pwd)/$1" "$2"
+function link {
+  set +e
+  rm "$2"
+  set -e
+  ln -s "$(pwd)/$1" "$2"
 }
 
 set -e
 set -x
 
-link aliases.zsh       ~/.oh-my-zsh/custom/aliases.zsh
-link .p10k.zsh         ~/.p10k.zsh
-link .gitconfig        ~/.gitconfig
+link aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
+link .p10k.zsh ~/.p10k.zsh
+link .gitconfig ~/.gitconfig
 link .gitignore_global ~/.gitignore_global
-link .zshrc            ~/.zshrc
-link .vimrc            ~/.vimrc
-
+link .zshrc ~/.zshrc
+link .vimrc ~/.vimrc
+link .config/atuin/config.toml ~/.config/atuin/config.toml
--- unlink.sh
+++ unlink.sh
@@ -3,21 +3,20 @@
 # removes the file in current filesystem
 # and replaces it with a new copy of the file in this directory
 
-function unlink
-{
-    set +e
-    rm "$1"
-    set -e
-    cp "$(pwd)/$2" "$1"
+function unlink {
+  set +e
+  rm "$1"
+  set -e
+  cp "$(pwd)/$2" "$1"
 }
 
 set -e
 set -x
 
 unlink ~/.oh-my-zsh/custom/aliases.zsh aliases.zsh
-unlink ~/.p10k.zsh                     .p10k.zsh
-unlink ~/.gitconfig                    .gitconfig
-unlink ~/.gitignore_global             .gitignore_global
-unlink ~/.zshrc                        .zshrc
-unlink ~/.vimrc                        .vimrc
-
+unlink ~/.p10k.zsh .p10k.zsh
+unlink ~/.gitconfig .gitconfig
+unlink ~/.gitignore_global .gitignore_global
+unlink ~/.zshrc .zshrc
+unlink ~/.vimrc .vimrc
+unlink ~/.config/atuin/config.toml .config/atuin/config.toml
